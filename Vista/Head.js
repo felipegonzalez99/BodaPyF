ada9936eed824a2b25bd4270a1179bd13158483d
felipegonzalez99 <43577352+felipegonzalez99@users.noreply.github.com>
2026-07-18 22:28:23 -0500

    Delete Vista/Head.js

--- Vista/Head.js
+++
@@ -1,9 +0,0 @@
-document.head.innerHTML += `
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Nuestra Boda - Invitación</title>
-  <meta property="og:title" content="Boda de Paola y Felipe">
-  <meta property="og:description" content="Te invitamos a compartir este día tan especial con nosotros">
-  <meta property="og:image" content="https://felipegonzalez99.github.io/BodaPyF/Imagenes/PREBODA_PR.jpg">
-
-`; 
