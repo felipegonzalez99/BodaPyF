9ca44bd2ac270e7104d781b6b28c7b0f7a68c8a9
felipegonzalez99 <43577352+felipegonzalez99@users.noreply.github.com>
2026-07-18 22:04:55 -0500

    Add files via upload

--- Head.js
+++ Head.js
@@ -1,11 +1,10 @@
-
 document.head.innerHTML += `
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Nuestra Boda - Invitación</title>
   <meta property="og:title" content="Boda de Paola y Felipe">
   <meta property="og:description" content="Te invitamos a compartir este día tan especial con nosotros">
-  <meta property="og:image" content="https://felipegonzalez99.github.io/BodaPyF/Imagenes/PREBODA5.jpg">
+  <meta property="og:image" content="https://felipegonzalez99.github.io/BodaPyF/Imagenes/PREBODA_PR.jpg">
   <meta property="og:url" content="https://felipegonzalez99.github.io/BodaPyF/">
   <meta property="og:type" content="website">
   <meta property="og:image:width" content="800">
@@ -19,5 +18,5 @@ document.head.innerHTML += `
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400..700&display=swap" rel="stylesheet">
   <link href="https://unpkg.com/aos@2.3.4/dist/aos.css" rel="stylesheet">
-  <link rel="shortcut icon" type="image/ico" href="Imagenes/Icono.jpg"/>
-`;
+  <link rel="shortcut icon" type="image/ico" href="../Imagenes/Icono.jpg"/>
+`; 
